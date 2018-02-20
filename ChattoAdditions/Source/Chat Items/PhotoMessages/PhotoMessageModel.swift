925e7e67bdf78f3e03e53ce7ab36a1dc33ce9f2d
“Alexander <alexsanderskywork@gmail.com>
2018-02-20 17:39:40 +0400

    added image path and compress image path

--- ChattoAdditions/Source/Chat Items/PhotoMessages/PhotoMessageModel.swift
+++ ChattoAdditions/Source/Chat Items/PhotoMessages/PhotoMessageModel.swift
@@ -1,18 +1,18 @@
 /*
  The MIT License (MIT)
-
+ 
  Copyright (c) 2015-present Badoo Trading Limited.
-
+ 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
-
+ 
  The above copyright notice and this permission notice shall be included in
  all copies or substantial portions of the Software.
-
+ 
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,12 +20,13 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  THE SOFTWARE.
-*/
+ */
+
 
 import UIKit
 
 public protocol PhotoMessageModelProtocol: DecoratedMessageModelProtocol {
-    var image: UIImage { get }
+    var image: UIImage? { get }
     var imageSize: CGSize { get }
 }
 
@@ -34,11 +35,17 @@ open class PhotoMessageModel<MessageModelT: MessageModelProtocol>: PhotoMessageM
         return self._messageModel
     }
     public let _messageModel: MessageModelT // Can't make messasgeModel: MessageModelT: https://gist.github.com/diegosanchezr/5a66c7af862e1117b556
-    public let image: UIImage
+    public var image: UIImage?
     public let imageSize: CGSize
-    public init(messageModel: MessageModelT, imageSize: CGSize, image: UIImage) {
+    public var imagePath: String?
+    public var compressImagePath: String?
+    
+    public init(messageModel: MessageModelT, imageSize: CGSize, image: UIImage?, imagePath: String?, compressImagePath: String?) {
         self._messageModel = messageModel
         self.imageSize = imageSize
         self.image = image
+        self.imagePath = imagePath
+        self.compressImagePath = compressImagePath
     }
+    
 }
